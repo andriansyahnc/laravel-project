1e85346840fda7de9978510017773b22a7597837
M Andriansyah Nurcahya <4andriansyah@gmail.com>
2022-04-24 04:43:15 +0700

    update classname

--- app/Console/Commands/RechargeRegularCron.php
+++ app/Console/Commands/RechargeRegularCron.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 use App\Services\UserPoint\UserPointRepository;
 use Illuminate\Contracts\Container\Container;
 
-class RechargePremiumCron extends Command
+class RechargeRegularCron extends Command
 {
     /**
      * The name and signature of the console command.
